5321f4c6b59497431a2d59144a21001f3f7959bb
Princearman777 <sarkerarman25@gmail.com>
2023-11-24 03:18:28 +0600

    Structure is created in this poject

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtBJyyKE3Jq6DJGMLnSxvNRNRVBpWIBjE",
+  authDomain: "blood-donation-1a2c5.firebaseapp.com",
+  projectId: "blood-donation-1a2c5",
+  storageBucket: "blood-donation-1a2c5.appspot.com",
+  messagingSenderId: "121199033066",
+  appId: "1:121199033066:web:ef7fa7d79b6a441198f593"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const auth =getaut
